91b0160b48c85fe1477170b1d80aa54f21693420
Jack Cheng <jackcheng2002@gmail.com>
2023-06-11 03:01:59 -0400

    new astro theme

---
+++ superior-satellite/src/content/post/unique-tags.md
@@ -0,0 +1,14 @@
+---
+title: "first post"
+publishDate: "6 June 2023"
+description: "this is the first post ever on this site, mainly just for testing."
+tags: ["first", "test"]
+---
+
+# h1
+
+## h2
+
+### h3
+
+First post.
---
+++ superior-satellite/src/data/constants.ts
@@ -0,0 +1,22 @@
+export const MENU_LINKS = [
+	{
+		title: "home",
+		path: "/",
+	},
+	{
+		title: "about",
+		path: "/about/",
+	},
+	{
+		title: "blog",
+		path: "/posts/",
+	},
+];
+
+// ! Remember to add your own socials
+export const SOCIAL_LINKS = {
+	github: "https://github.com/7-jack",
+	// twitter: "",
+	// discord: "https://github.com/chrismwilliams/astro-cactus",
+	email: "jackcheng2002@gmail.com",
+};
